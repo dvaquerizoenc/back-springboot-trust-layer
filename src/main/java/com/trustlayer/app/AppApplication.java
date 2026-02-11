773bba78c120506c944bebe1ab2fc2a8f35cf289
David Vaquerizo <davidvaquerizoenc@gmail.com>
2026-02-11 13:45:37 +0100

    Agregada conexion a base de datos

--- src/main/java/com/trustlayer/app/AppApplication.java
+++ src/main/java/com/trustlayer/app/AppApplication.java
@@ -2,12 +2,14 @@ package com.trustlayer.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.origin.SystemEnvironmentOrigin;
 
 @SpringBootApplication
 public class AppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
-	}
+        System.out.println("hola iniciado sprignboot");
+    }
 
 }
